cd728402509bfb18b0024648775d4413fc4e0daf
Carlos Santana <carlosefdesantana@gmail.com>
2023-01-05 18:33:38 -0300

    feat: Adicionando roteamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Navigate, Route, Routes } from "react-router-dom"
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/pagina-inicial" element={<p>Teste rota</p>} />
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} /> {/* Redirect to home page */}
+        </Routes>
+    )
+}
