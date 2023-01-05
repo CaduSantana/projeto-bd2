ceadc3477866fd38d37e859c272bdccd3d8bc8bb
Carlos Santana <carlosefdesantana@gmail.com>
2023-01-05 18:48:06 -0300

    feat: adicionando Material UI 5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 export const AppRoutes = () => {
     return (
         <Routes>
             {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/pagina-inicial" element={<p>Teste rota</p>} />
+            <Route path="/pagina-inicial" element={<Button> Teste button</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} /> {/* Redirect to home page */}
         </Routes>
     )
