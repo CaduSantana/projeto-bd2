30b3702c40e0429412f4f0e5766b4f4c68372949
GuiCT <gtomiasi@gmail.com>
2023-01-06 14:34:38 -0300

    feat: Drawer responsivo a partir do breakpoint sm, utilizando DrawerContext para armazenamento do estado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-    const {toggleTheme} = useAppThemeContext();
+    const { toggleDrawerOpen } = useDrawerContext();
     return (
         <Routes>
             {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/pagina-inicial" element={<Button variant='contained' color="primary" onClick={toggleTheme}> Teste button</Button>} />
+            <Route path="/pagina-inicial" element={<Button variant='contained' color="primary" onClick={ toggleDrawerOpen }> Toggle drawer</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} /> {/* Redirect to home page */}
         </Routes>
     );
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,25 +1,30 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import { useDrawerContext } from '../../contexts';
 
 export const MenuLateral: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  const theme = useTheme();  
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
   
   return (
     <>
-      <Drawer open={ true } variant='permanent'>
+      <Drawer open={ isDrawerOpen } variant={ smDown ? 'temporary' : 'permanent' } onClose={ toggleDrawerOpen }>
         <Box
         width={ theme.spacing(28) } height="100%" display="flex" flexDirection="column">
           <Box width="100%" height={ theme.spacing(20) } display="flex" alignItems="center" justifyContent="center">
             <Avatar sx={{ height: theme.spacing(12), width: theme.spacing(12) }}>U</Avatar>
           </Box>
+
           <Divider />
+
           <Box flex="1">
             <List component="nav">
               <ListItemButton>
                 <ListItemIcon>
                   <Icon>home</Icon>
                 </ListItemIcon>
+                
                 <ListItemText primary="Página inicial" />
               </ListItemButton>
             </List>
@@ -27,7 +32,7 @@ export const MenuLateral: React.FC<{ children?: React.ReactNode }> = ({ children
         </Box>
       </Drawer>
 
-      <Box height="100vh" marginLeft={ theme.spacing(28) }>
+      <Box height="100vh" marginLeft={ smDown ? 0 : theme.spacing(28) }>
         { children }
       </Box>
     </>
