72d5b71a4fcb1260e01e0de2ac97cd90fb8a6c8a
GuiCT <gtomiasi@gmail.com>
2023-01-06 20:40:24 -0300

    atualizando rota padrão

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@mui/material';
 import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Dashboard } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([
@@ -18,8 +18,8 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      {/* <Route path='/' element={<Home />} /> */}
-      <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' onClick={ toggleDrawerOpen }> Toggle drawer</Button>} />
+      <Route path='/pagina-inicial' element={<Dashboard/>} />
+
       <Route path='*' element={<Navigate to='/pagina-inicial' />} /> {/* Redirect to home page */}
     </Routes>
   );
