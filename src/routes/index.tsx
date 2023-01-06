69458f480f2eeb6a4944e4f72887a7e585f2dd20
GuiCT <gtomiasi@gmail.com>
2023-01-06 19:18:39 -0300

    feat: atualizando rotas para incluir as opções de Drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-    const { toggleDrawerOpen } = useDrawerContext();
-    return (
-        <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/pagina-inicial" element={<Button variant='contained' color="primary" onClick={ toggleDrawerOpen }> Toggle drawer</Button>} />
-            <Route path="*" element={<Navigate to="/pagina-inicial" />} /> {/* Redirect to home page */}
-        </Routes>
-    );
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      }
+    ]);
+  }, []);
+
+  return (
+    <Routes>
+      {/* <Route path='/' element={<Home />} /> */}
+      <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' onClick={ toggleDrawerOpen }> Toggle drawer</Button>} />
+      <Route path='*' element={<Navigate to='/pagina-inicial' />} /> {/* Redirect to home page */}
+    </Routes>
+  );
 };
