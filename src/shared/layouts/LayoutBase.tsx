898b0e79b7615f91419940c0b3ca3c2581b525c5
GuiCT <gtomiasi@gmail.com>
2023-01-06 20:40:00 -0300

    feat: adicionand página de dashbord e layout base

---
+++ src/shared/layouts/LayoutBase.tsx
@@ -0,0 +1,37 @@
+import { Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+import { useDrawerContext } from '../contexts';
+
+interface ILayoutBaseProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const LayoutBase: React.FC<ILayoutBaseProps> = ({ title, children }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const { toggleDrawerOpen } = useDrawerContext();
+
+  return (
+    <Box height="100%" display="flex" flexDirection="column" gap={1}>
+      <Box padding={1} height={theme.spacing(12)} display="flex" alignItems="center" gap={1}>
+        {smDown ?
+        (<IconButton onClick={ toggleDrawerOpen }>
+          <Icon>menu</Icon>
+        </IconButton>) : undefined}
+
+        <Typography variant='h5'>
+          { title }
+        </Typography>
+      </Box>
+      
+      <Box>
+        Barra de ferramentas
+      </Box>
+
+      <Box>
+        { children }
+      </Box>
+    </Box>
+  );
+};
