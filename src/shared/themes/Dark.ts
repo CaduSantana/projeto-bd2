7e0d0b0c8969fccb79c589f0b73f0b1018388ffc
GuiCT <gtomiasi@gmail.com>
2023-01-07 01:30:43 -0300

    fix: atualizando modo escuro

--- src/shared/themes/Dark.ts
+++ src/shared/themes/Dark.ts
@@ -3,21 +3,27 @@ import { cyan, yellow } from '@mui/material/colors';
 
 export const DarkTheme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       main: yellow[700],
       dark: yellow[800],
       light: yellow[500],
-      contrastText: '#000',
+      contrastText: '#ffffff',
     },
     secondary: {
       main: cyan[500],
       dark: cyan[700],
       light: cyan[300],
-      contrastText: '#000',
+      contrastText: '#ffffff',
     },
     background: {
       default: '#202124',
       paper: '#303134',
     },
   },
+  typography: {
+    allVariants: {
+      color: 'white'
+    }
+  }
 });
