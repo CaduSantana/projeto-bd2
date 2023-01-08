b711c0f70e3cf8e37d53b7c76af448bebbd3c849
GuiCT <gtomiasi@gmail.com>
2023-01-08 00:21:45 -0300

    feat: tabela genérica
    destinado para uso no futuro

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './menu-lateral/MenuLateral';
 export * from './barra-de-ferramentas/BarraDeFerramentas';
 export * from './barra-de-selecao/BarraDeSelecao';
 export * from './autocomplete-combobox/AutoCompleteComboBox';
+export * from './tabela/Tabela';
