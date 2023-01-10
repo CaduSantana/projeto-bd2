b8405d4d071d115523904d0ec3c42df1fe8f18fd
GuiCT <gtomiasi@gmail.com>
2023-01-10 15:58:28 -0300

    feat: armazenando tema no LocalStorage

--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -5,38 +5,45 @@ import { DarkTheme, LightTheme } from '../themes';
 import { Box } from '@mui/system';
 
 interface ThemeContextData {
-    themeName: 'light' | 'dark';
-    toggleTheme: () => void;
+  themeName: 'light' | 'dark';
+  toggleTheme: () => void;
 }
 
-interface ThemeProviderProps{
-    children: React.ReactNode;
+interface ThemeProviderProps {
+  children: React.ReactNode;
 }
 
 const ThemeContext = createContext({} as ThemeContextData);
 
 export const useAppThemeContext = () => {
-    return useContext(ThemeContext);
+  return useContext(ThemeContext);
 };
 
 export const AppThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-    const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
-    const toggleTheme = useCallback(() => {
-        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
-    }, []);
+  const getThemeFromLocalStorage = () => {
+    const themeName = localStorage.getItem('themeName');
+    if (themeName === 'light' || themeName === 'dark') return themeName;
+    return 'light';
+  };
 
-    const theme = useMemo(() => {
-        if (themeName === 'light') return LightTheme;
-        return DarkTheme;
-    }, [themeName]);
+  const [themeName, setThemeName] = useState<'light' | 'dark'>(getThemeFromLocalStorage());
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) => (oldThemeName === 'light' ? 'dark' : 'light'));
+    localStorage.setItem('themeName', themeName === 'light' ? 'dark' : 'light');
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{themeName, toggleTheme}}>
-            <ThemeProvider theme={theme}>
-                <Box width={'100vw'} height={'100vh'} bgcolor={theme.palette.background.default}>
-                    {children}
-                </Box>
-            </ThemeProvider>
-        </ThemeContext.Provider>
-    );
+  const theme = useMemo(() => {
+    if (themeName === 'light') return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box width={'100vw'} height={'100vh'} bgcolor={theme.palette.background.default}>
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 };
