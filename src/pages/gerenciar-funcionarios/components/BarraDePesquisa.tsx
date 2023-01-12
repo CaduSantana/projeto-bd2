c601d3cc78aea53d14effa7b251f21a91efaac02
GuiCT <gtomiasi@gmail.com>
2023-01-12 01:12:01 -0300

    feat: criando componentes utilizados na página
    "Copiados" da página de gerenciar veículos
    Tenho que ver se é possível melhorar a modularização posteriormente

---
+++ src/pages/gerenciar-funcionarios/components/BarraDePesquisa.tsx
@@ -0,0 +1,27 @@
+import { Button, Icon, Paper, TextField, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+
+interface IBarraDePesquisaProps {
+  onAdicionar: () => void;
+}
+
+export const BarraDePesquisa: React.FC<IBarraDePesquisaProps> = ({ onAdicionar }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      height={theme.spacing(5)}
+      padding={1}
+      paddingX={2}
+      display='flex'
+      gap={1}
+      alignItems='center'
+      component={Paper}
+    >
+      <TextField size='small' placeholder='Pesquisar...' fullWidth />
+      <Button variant='contained' endIcon={<Icon>library_add</Icon>} onClick={onAdicionar}>
+        Adicionar
+      </Button>
+    </Box>
+  );
+};
