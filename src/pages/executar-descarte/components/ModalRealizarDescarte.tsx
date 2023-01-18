ad43582353889050cdf775c6621038e022a20c9e
GuiCT <gtomiasi@gmail.com>
2023-01-18 00:22:29 -0300

    feat: modal de seleção de veículo para um func.

--- src/pages/executar-descarte/components/ModalRealizarDescarte.tsx
+++ src/pages/executar-descarte/components/ModalRealizarDescarte.tsx
@@ -1,6 +1,7 @@
-import { useMemo } from 'react';
+import { MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import { useMemo, useState } from 'react';
 import { ModalWrapper, Tabela } from '../../../shared/components';
-import { getExemploFuncionario } from '../../../shared/services/api';
+import { getExemploFuncionario, getExemploVeiculo } from '../../../shared/services/api';
 import { useExecutarDescarteContext } from '../ExecutarDescarteContext';
 
 const TabelaFuncionariosDisponiveis: React.FC = () => {
@@ -17,25 +18,29 @@ const TabelaFuncionariosDisponiveis: React.FC = () => {
   );
 
   return (
-    <Tabela
-      cabecalho={['Nome completo', 'CPF', 'Email']}
-      alinhamentos={['left', 'left', 'right']}
-      linhas={linhasTabela}
-      acoes={[
-        {
-          icon: 'add',
-          funcao: (linhaIndex: number) => {
-            funcionariosSelecionados.addFuncionario(funcionarios[linhaIndex]);
+    <>
+      <Typography variant='h5'>Funcionários</Typography>
+      <Tabela
+        cabecalho={['Nome completo', 'CPF', 'Email']}
+        alinhamentos={['left', 'left', 'right']}
+        linhas={linhasTabela}
+        acoes={[
+          {
+            icon: 'add',
+            funcao: (linhaIndex: number) => {
+              funcionariosSelecionados.addFuncionario(funcionarios[linhaIndex]);
+            },
           },
-        },
-      ]}
-    />
+        ]}
+      />
+    </>
   );
 };
 
 const ListaFuncionariosSelecionados: React.FC = () => {
   const funcionarios = [getExemploFuncionario()];
-  const { funcionariosSelecionados } = useExecutarDescarteContext();
+
+  const { funcionariosSelecionados, modalSelecionarVeiculo } = useExecutarDescarteContext();
   const linhasTabela = useMemo(
     () =>
       funcionariosSelecionados.value.map(({ funcionario, veiculoUtilizado }) => [
@@ -48,21 +53,74 @@ const ListaFuncionariosSelecionados: React.FC = () => {
   );
 
   return funcionariosSelecionados.value.length > 0 ? (
-    <Tabela
-      cabecalho={['Nome completo', 'CPF', 'Email', 'Veículo utilizado']}
-      alinhamentos={['left', 'left', 'right']}
-      linhas={linhasTabela}
-      acoes={[
-        {
-          icon: 'delete',
-          funcao: (linhaIndex: number) => {
-            funcionariosSelecionados.removeFuncionario(funcionarios[linhaIndex]);
+    <>
+      <Typography variant='h5'>Funcionários selecionados</Typography>
+      <Tabela
+        cabecalho={['Nome completo', 'CPF', 'Email', 'Veículo utilizado']}
+        alinhamentos={['left', 'left', 'right']}
+        linhas={linhasTabela}
+        acoes={[
+          {
+            icon: 'delete',
+            funcao: (linhaIndex: number) => {
+              funcionariosSelecionados.removeFuncionario(funcionarios[linhaIndex]);
+            },
           },
-        },
-      ]}
-    />
+          {
+            icon: 'local_shipping',
+            funcao: (linhaIndex: number) => {
+              modalSelecionarVeiculo.setFuncionario(funcionarios[linhaIndex]);
+              modalSelecionarVeiculo.setOpen(true);
+            },
+          },
+        ]}
+      />
+    </>
   ) : (
     <></>
+  );
+};
+
+const ModalSelecionarVeiculo: React.FC = () => {
+  const veiculos = [getExemploVeiculo()];
+
+  const [uuidVeiculoSelecionado, setUuidVeiculoSelecionado] = useState<string>('');
+
+  const {
+    modalSelecionarVeiculo: { open, setOpen, funcionario },
+    funcionariosSelecionados: { associateVeiculo },
+  } = useExecutarDescarteContext();
+
+  function handleChangeVeiculoSelecionado(e: SelectChangeEvent<string>) {
+    setUuidVeiculoSelecionado(e.target.value);
+  }
+
+  function handleCloseModal() {
+    // Verifica se um veículo foi selecionado
+    // Se sim, associa esse veículo ao funcionário
+    if (uuidVeiculoSelecionado && funcionario) {
+      const veiculoSelecionado = veiculos.find((veiculo) => veiculo.uuid === uuidVeiculoSelecionado);
+      if (veiculoSelecionado) {
+        associateVeiculo(funcionario, veiculoSelecionado);
+      }
+    }
+    setOpen(false);
+  }
+
+  return (
+    <ModalWrapper open={open} onClose={handleCloseModal}>
+      <Typography variant='h5'>
+        Selecione o veículo utilizado por {funcionario?.nome} {funcionario?.sobrenome}
+      </Typography>
+      <Select value={uuidVeiculoSelecionado} onChange={handleChangeVeiculoSelecionado}>
+        {veiculos.map((veiculo) => (
+          <MenuItem
+            key={veiculo.uuid}
+            value={veiculo.uuid}
+          >{`${veiculo.placa} - ${veiculo.tipo} (${veiculo.capacidade})`}</MenuItem>
+        ))}
+      </Select>
+    </ModalWrapper>
   );
 };
 
@@ -71,13 +129,15 @@ export const ModalRealizarDescarte: React.FC = () => {
 
   return (
     <ModalWrapper
-      open={modalRealizarDescarte.value}
-      onClose={() => {
-        modalRealizarDescarte.setValue(false);
+      open={modalRealizarDescarte.open}
+      onClose={function () {
+        modalRealizarDescarte.setOpen(false);
       }}
     >
       <TabelaFuncionariosDisponiveis />
       <ListaFuncionariosSelecionados />
+
+      <ModalSelecionarVeiculo />
     </ModalWrapper>
   );
 };
--- src/pages/executar-descarte/components/TabelaDescartes.tsx
+++ src/pages/executar-descarte/components/TabelaDescartes.tsx
@@ -2,23 +2,26 @@ import { Tabela } from '../../../shared/components';
 import { getExemploDescarte } from '../../../shared/services/api';
 import { formatDate } from '../../../shared/services';
 import { useExecutarDescarteContext } from '../ExecutarDescarteContext';
+import { useMemo } from 'react';
 
 export const TabelaDescartes = () => {
   const descartes = [getExemploDescarte()];
   const { modalRealizarDescarte } = useExecutarDescarteContext();
 
+  const linhasTabela: string[][] = useMemo(() => descartes.map((descarte) => [
+    `${descarte.solicitante.nome} ${descarte.solicitante.sobrenome}`,
+    formatDate(descarte.solicitadoEm),
+    descarte.produtosDescartados
+      .map((itemProduto) => `${itemProduto.produto.nome} (${itemProduto.quantidade})`)
+      .join(', '),
+    `${descarte.origem.rua}, ${descarte.origem.numero} - ${descarte.origem.bairro}`,
+  ]), [descartes]);
+
   return (
     <Tabela
       cabecalho={['Solicitado por', 'Momento da solicitação', 'Itens descartados', 'Local de origem']}
       alinhamentos={['left', 'left', 'center', 'left']}
-      linhas={descartes.map((descarte) => [
-        `${descarte.solicitante.nome} ${descarte.solicitante.sobrenome}`,
-        formatDate(descarte.solicitadoEm),
-        descarte.produtosDescartados
-          .map((itemProduto) => `${itemProduto.produto.nome} (${itemProduto.quantidade})`)
-          .join(', '),
-        `${descarte.origem.rua}, ${descarte.origem.numero} - ${descarte.origem.bairro}`,
-      ])}
+      linhas={linhasTabela}
       acoes={[
         {
           icon: 'map',
@@ -28,8 +31,8 @@ export const TabelaDescartes = () => {
         },
         {
           icon: 'auto_delete',
-          funcao: (linhaIndex) => {
-            modalRealizarDescarte.setValue(true);
+          funcao: () => {
+            modalRealizarDescarte.setOpen(true);
           },
         },
       ]}
